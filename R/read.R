a839e3478a4eb2b38985894bfa233d12fc32535e
Radoslaw Panczak <31648642+RPanczak@users.noreply.github.com>
2023-11-06 15:57:11 +0100

    read in new arrivals & better desc

--- R/read.R
+++ R/read.R
@@ -1,6 +1,7 @@
 library(readr)
 library(dplyr)
 library(lubridate)
+library(stringr)
 
 import::from("sjmisc", "frq")
 
@@ -88,7 +89,17 @@ data <-
           mutate(year = 2022, quarter = quarter(ymd("2022-07-01"))),
         
         read_csv("data-raw/complaints/cars-srsa-open-data-animal-related-complaints-oct-to-dec-2022.csv.zip") %>%
-          mutate(year = 2022, quarter = quarter(ymd("2022-10-01")))
+          mutate(year = 2022, quarter = quarter(ymd("2022-10-01"))),
+        
+        read_csv("data-raw/complaints/cars-srsa-open-data-animal-related-complaints-jan-to-mar-2023.csv.zip") %>%
+          mutate(year = 2023, quarter = quarter(ymd("2022-01-01"))),
+        
+        read_csv("data-raw/complaints/cars-srsa-open-data-animal-related-complaints-apr-to-jun-2023.csv.zip") %>%
+          mutate(year = 2023, quarter = quarter(ymd("2022-04-01"))),
+        
+        read_csv("data-raw/complaints/cars-srsa-open-data-animal-related-complaints-jul-to-sep-2023.csv.zip") %>%
+          mutate(year = 2023, quarter = quarter(ymd("2022-07-01")))
+        
       )  %>% 
         select(-Index) 
       
@@ -116,14 +127,36 @@ data <-
         suburb = `Suburb`)
   ) %>% 
   relocate(year, quarter) %>% 
-  arrange(year, quarter) %>% 
+  arrange(year, quarter)
+
+data %>% 
+  filter(! nature == "Animal") %>% 
+  nrow()
+  
+data <- data %>% 
   filter(nature == "Animal") %>% 
-  select(-nature)
+  select(-nature) 
+
+data %>% 
+  filter(type == "Attack" & reporting_level == "Not An Attack") %>% 
+  nrow()
+
+data <- data %>% 
+  filter(! (type == "Attack" & reporting_level == "Not An Attack") )
+  
 
 names(data)
+
+data %>% 
+  mutate(date = str_c(year, "-", quarter)) %>% 
+  {length(unique(.$date))}
+
+data %>% 
+  mutate(date = str_c(year, "-", quarter)) %>% 
+  frq(date)
 
 frq(data, type, sort.frq = "desc")
 frq(data, reporting_level, sort.frq = "desc")
 
 temp <- data %>% 
-janitor::tabyl(reporting_level, type)
+  janitor::tabyl(reporting_level, type)
